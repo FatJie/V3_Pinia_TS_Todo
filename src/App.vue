e5b620f2b3ef36f930e6982ca7e42cffddc8b422
Fatjie <372761781@qq.com>
2023-08-31 09:46:24 +0800

    初始化页面静态结构

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="todoapp">
+    <TodoHeader />
+    <!-- This section should be hidden by default and shown when there are todos -->
+    <TodoMain />
+    <!-- This footer should be hidden by default and shown when there are todos -->
+    <TodoFooter />
+  </section>
+</template>
+
+<script setup lang="ts">
+import TodoHeader from './components/TodoHeader.vue';
+import TodoMain from './components/TodoMain.vue';
+import TodoFooter from './components/TodoFooter.vue';
+</script>
