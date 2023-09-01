0d323229404eeb272bdf33d39ae63bbc4d004de1
Fatjie <372761781@qq.com>
2023-09-01 09:02:31 +0800

    更新 itemTodo

--- src/components/ItemTodo.vue
+++ src/components/ItemTodo.vue
@@ -5,7 +5,7 @@
       <label @dblclick="handleDbclick">{{ item.name }}</label>
       <button class="destroy" @click="delTodo(item.id)"></button>
     </div>
-    <input class="edit" placeholder="Create a TodoMVC template" ref="inputRef" @blur="hanleBlur"/>
+    <input class="edit" placeholder="Create a TodoMVC template" ref="inputRef" @blur="hanleBlur" v-model="curName" @keyup="handelKeyUp"/>
   </li>
 </template>
 <script setup lang='ts'>
@@ -13,7 +13,7 @@ import useStore from '../store'
 import { ITodoItem } from '../types/data';
 import { nextTick, ref } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   item: ITodoItem
 }>()
 
@@ -21,15 +21,31 @@ const { main } = useStore()
 const { getTodos, delTodo, updateToodo } = main
 getTodos()
 
+const curName = ref('')
 const isEditing = ref(false)
 const inputRef = ref<HTMLInputElement>()
 const handleDbclick = () => {
   isEditing.value = true
+  curName.value = props.item.name
   nextTick(() => inputRef.value?.focus())
 }
 
 const hanleBlur = () => {
   isEditing.value = false
+  curName.value = ''
+}
+
+const handelKeyUp = (e: KeyboardEvent) => {
+  if(e.key === 'Enter') {
+    if(!curName.value.trim()) return alert('不能为空')
+    updateToodo(props.item.id, 'name', curName.value)
+    hanleBlur()
+  } else if(e.key === 'Escape') {
+    hanleBlur()
+    return
+  } else {
+
+  }
 }
 </script>
 
